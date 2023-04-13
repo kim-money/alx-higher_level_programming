9430bb5eb66893772bfa86cf14333563b6b45857
kim-money <wangingimoses102@gmail.com>
2023-04-13 00:10:43 +0000

    2,3,4

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""This module defines a string-to-JSON function"""
+"""defines a string-to-JSON fn"""
 import json
 
 
 def to_json_string(my_obj):
+    """Return the JSON representation of a string object."""
     return json.dumps(my_obj)
--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-"""defines a json function"""
 import json
 
 
