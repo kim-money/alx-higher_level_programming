1c3824724013c423195664efacf8c6f7bbb6e110
kim-money <wangingimoses102@gmail.com>
2023-04-14 20:12:01 +0000

    tasks

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3.5
+"""
+
+Module composed by a function that multiplies 2 matrices
+
+"""
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """ Function that multiplies 2 matrices
+
+    """
+
+    return (np.matmul(m_a, m_b))
