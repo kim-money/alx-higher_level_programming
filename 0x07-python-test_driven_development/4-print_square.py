961ca848fb5d4983f99b8b0050a956128f8078b1
key-money <wangingimoses102@gmail.com>
2023-06-06 20:57:29 +0000

    the rest

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+
+This module is composed by a function that prints a square with the character #
+
+"""
+
+
+def print_square(size):
+    """ Function that prints a square with the character #
+        TypeError: If size is not an integer number
+
+    """
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    """
+       iteration
+    """
+    for i in range(size):
+        print("#" * size)
